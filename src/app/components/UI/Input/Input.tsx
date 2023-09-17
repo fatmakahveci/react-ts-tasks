6b0481ebeb1d86a08ebbec9f4a0e09d2ae832b1e
fatmakhv <fatmaba@gmail.com>
2023-09-17 10:54:46 +0300

    Input is added.

---
+++ src/app/components/UI/Input/Input.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { InputProps } from '@/shared/types';
+import { FC, forwardRef } from 'react';
+import './Input.css';
+
+const Input: FC<InputProps> = forwardRef(({label, input}, ref) => {
+  return (
+    <div className="input">
+      <label htmlFor={input["id"]}>{label}</label>
+      <input
+        id={`task_${input["id"]}`}
+        type={input["type"]}
+        ref={ref}
+      />
+    </div>
+  )
+});
+
+export default Input;
